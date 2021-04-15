8cc65c63cd0f22755f30432cdaafb1f83226bc16
Saifullah Chowdhury <saifullahchy0177@gmail.com>
2021-04-16 03:34:16 +0600

    final app done

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { ChatEngine} from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
+const projectID = '0ab0c493-637c-4308-b557-4b6cbb5b2e79';
 
 const App = () => {
-    return(
+
+    if(!localStorage.getItem('username')) return <LoginForm/>
+
+    return (
         <ChatEngine
-            height = "100vh"
-            projectID = "0ab0c493-637c-4308-b557-4b6cbb5b2e79"
-            userName = "saifullahChowdhury"
-            userSecret = "1234"
-            renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
-
+          height="100vh"
+          projectID={projectID}
+          userName={localStorage.getItem('username')}
+          userSecret={localStorage.getItem('password')}
+          renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+          onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
-    );
-}
-
-
+      );
+    }
 export default App;
 
 
